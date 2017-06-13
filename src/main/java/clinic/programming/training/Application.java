aa246b6362055631945f5658245addf5c5b63089
libranpuneet <libranpuneet@users.noreply.github.com>
2017-06-13 10:06:06 +0530

    used StringUtils & added dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,12 +2,19 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application constructor");
     }
 
+	public int countWords(String word) {
+		
+		String[] seperateWords=StringUtils.split(word,' ');
+		return (seperateWords ==null) ? 0: seperateWords.length;
+	}
+	
 	
 	public void greet(){
 	List<String> greetings= new ArrayList<String>();
@@ -25,5 +32,8 @@ public class Application {
     	System.out.println ("Starting Application");
 	   Application app = new Application();
 	   app.greet();
+	      int totalwordcount =app.countWords("Apple was eaten by me");
+	   System.out.println("total word count :>>  " +totalwordcount);
+	
     }
 }
