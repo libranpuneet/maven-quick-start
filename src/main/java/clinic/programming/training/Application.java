5b9de7ed77759c2fa90c179c65e58ee0b380c100
libranpuneet <libranpuneet@users.noreply.github.com>
2017-06-13 09:42:20 +0530

    Udpating files

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,29 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println ("Inside Application constructor");
     }
 
+	
+	public void greet(){
+	List<String> greetings= new ArrayList<String>();
+	greetings.add("hello you");
+	
+	for (String s: greetings ){
+		System.out.println("Greetings : >> " + s);
+	}
+		
+	}
+	
+	
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	   Application app = new Application();
+	   app.greet();
     }
 }
